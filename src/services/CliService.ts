e4305e2d2ed8e1ceec2716093f985b649c152658
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2024-01-30 15:17:05 +0300

    Cli runnable

--- src/services/CliService.ts
+++ src/services/CliService.ts
@@ -1,5 +1,7 @@
 import {Uri, workspace} from "vscode";
 import * as os from "os";
+import * as fs from "fs";
+import * as child_process from 'child_process';
 import AdmZip from "adm-zip";
 import {posix} from "path";
 import {api} from "../api";
@@ -40,7 +42,11 @@ export class CliService {
     }
 
     private _zipSuffix(): string {
-        return this._isWindows() ? 'exe.zip' : 'zip';
+        return this._isWindows() ? '.exe.zip' : '.zip';
+    }
+
+    private _fileExtension(): string {
+        return this._isWindows() ? '.exe' : '';
     }
 
     private _cliZipFileName(): string | null {
@@ -49,22 +55,36 @@ export class CliService {
         const zip = this._zipSuffix();
 
         if (!os || !cpu) {return null;}
-        return `cli-${os}-${cpu}.${zip}`;
+        return `cli-${os}-${cpu}${zip}`;
     }
 
-    private _getDownloadCliUrl(): string | null {
-        const zipFileName = this._cliZipFileName();
-        return !!zipFileName ? `${CLI_URL}/${CLI_VERSION}/${zipFileName}` : null;
+    private _cliFileName(): string | null {
+        const os = this._osSuffix();
+        const cpu = this._cpuSuffix();
+        const extension = this._fileExtension();
+
+        if (!os || !cpu) {return null;}
+        return `cli-${os}-${cpu}${extension}`;
+    }
+
+    private _getCliFileUri(): Uri {
+        const cliFilename = this._cliFileName() || '';
+        const storageUri = safeCtx().globalStorageUri;
+        return storageUri.with({path: posix.join(storageUri.path, CLI_VERSION, cliFilename)});
+    }
+
+    private _getDownloadCliUrl(zipFileName: string): string {
+        return `${CLI_URL}/${CLI_VERSION}/${zipFileName}`;
     }
 
     private async _downloadZippedCli(zipUrl: string): Promise<Buffer | null> {
         return await api.cliRepository.apiDowloadZippedCli(zipUrl);
     }
 
-    private async _writeZippedCli(writeData: Buffer): Promise<Uri> {
+    private async _writeZippedCli(writeData: Buffer, zipFileName: string): Promise<Uri> {
         const storageUri = safeCtx().globalStorageUri;
 
-        const fileUri = storageUri.with({path: posix.join(storageUri.path, CLI_VERSION, 'cli-darwin-arm64.zip')});
+        const fileUri = storageUri.with({path: posix.join(storageUri.path, CLI_VERSION, zipFileName)});
         await workspace.fs.writeFile(fileUri, writeData);
 
         return fileUri;
@@ -76,22 +96,38 @@ export class CliService {
         
         const zip = new AdmZip(zipUri.fsPath);
         zip.extractAllTo(folderUri.fsPath, true);
+
+        const fileUri = this._getCliFileUri();
+        fs.chmodSync(fileUri.fsPath, 0o755);
     }
 
     public async checkAndIntall(): Promise<void> {
-        const downloadUrl = this._getDownloadCliUrl();
-        if (!downloadUrl) {
-            return;
-        }
+        const zipFileName = this._cliZipFileName();
+        if (!zipFileName) {return;}
+
+        const downloadUrl = this._getDownloadCliUrl(zipFileName);
 
         const writeData = await this._downloadZippedCli(downloadUrl);
-        if (!writeData) {
-            return;
-        }
+        if (!writeData) {return;}
 
         // ~/Library/Application%20Support/Code/User/globalStorage
         //  /undefined_publisher.nau-time-tracker/v1.0.2/cli-darwin-arm64.zip
-        const fileUri = await this._writeZippedCli(writeData);
+        const fileUri = await this._writeZippedCli(writeData, zipFileName);
         this._unzipCli(fileUri);
+    }
+
+    public pushEvent(): void {
+        const fileUri = this._getCliFileUri();
+
+        const proc = child_process.execFile(fileUri.fsPath, [], {}, (error, stdout, stderr) => {
+            console.log('execFile', error, stdout, stderr);
+            console.log(stdout, stderr);
+        });
+
+        if (proc.stdout) {
+            proc.stdout.on('data', (data: string | null) => {
+                console.log('execFile data', data);
+            });
+        }
     }
 }
